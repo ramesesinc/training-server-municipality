b530ecebcdd8d244482067085c01c9e3af2b42b2
worgflores <worgflores@gmail.com>
2022-11-08 15:14:22 +0800

    - added initial database scripts - added services - added etracs web - added download modules

---
+++ services/modules/rptis/rameses-gov-etracs-rptis-landtax-services.jar/sql/landtax_repost_shares.sql
@@ -0,0 +1,39 @@
+[clearForRepostEntries]
+delete from  cashreceipt_rpt_share_forposting_repost
+
+[insertReceiptToRepost]
+insert into cashreceipt_rpt_share_forposting_repost (
+	objid,
+	rptpaymentid,
+	receiptid,
+	receiptdate,
+	rptledgerid,
+	error,
+	receipttype
+)
+select 
+	CONCAT(rp.objid, '-', cr.objid) as objid,
+	rp.objid as rptpaymentid,
+	cr.objid as receiptid, 
+	cr.receiptdate,
+	rp.refid as rptledgerid,
+	0 as error,
+	'ONLINE' as receipttype
+from collectionvoucher cv
+inner join remittance rem on cv.objid = rem.collectionvoucherid
+inner join cashreceipt cr on rem.objid = cr.remittanceid
+inner join rptpayment rp on cr.objid = rp.receiptid
+where cv.controldate >= $P{startdate} and cv.controldate < $P{enddate}
+
+[deleteRptSharesToRepost]
+delete from rptpayment_share 
+where parentid in (select rptpaymentid from cashreceipt_rpt_share_forposting_repost)
+
+[deleteCashReceiptSharesToRepost]
+delete from cashreceipt_share 
+where receiptid in (
+	select distinct receiptid from cashreceipt_rpt_share_forposting_repost
+)
+
+[findRemainingCount]
+select count(*) as remaining from cashreceipt_rpt_share_forposting_repost
